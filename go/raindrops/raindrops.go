293c61331fe5bf3fc6999997491a0557f8cb10f1
Greg <23417426+8032@users.noreply.github.com>
2021-01-14 08:56:57 +0000

    Optimise num conversion and add missing docstring

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,22 +1,22 @@
 package raindrops
 
-import "fmt"
+import "strconv"
 
-
+// Convert Calculate the factors of the integer where it is a factor 3, 5 or 7 and generates the required string for the sound of raindrops
 func Convert(num int) string {
 	output := ""
 
-	if num % 3 == 0 {
+	if num%3 == 0 {
 		output += "Pling"
 	}
-	if num % 5 == 0 {
+	if num%5 == 0 {
 		output += "Plang"
 	}
-	if num % 7 == 0 {
+	if num%7 == 0 {
 		output += "Plong"
 	}
-	if output == ""{
-		output = fmt.Sprintf("%d", num)
+	if output == "" {
+		output = strconv.Itoa(num)
 	}
 
 	return output
